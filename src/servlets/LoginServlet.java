57527e64507ac255a430a040f3fceb49d5cf9fea
Natalya Kosenko <natalya@ntuclink.com.sg>
2018-09-29 14:47:24 +0800

    Add doGet into Login servlet

--- src/servlets/LoginServlet.java
+++ src/servlets/LoginServlet.java
@@ -47,7 +47,7 @@ public class LoginServlet extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 	}
 
 }
